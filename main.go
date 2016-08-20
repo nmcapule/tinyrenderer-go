906b43b92d1b46ddb7603ebc2bd54d088698ffd5
Nathaniel M. Capule <nmcapule@gmail.com>
2016-08-20 22:13:57 +0800

    Add simple pixel drawing.

--- main.go
+++ main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
-    "github.com/ftrvxmtrx/tga"
-    "image"
-    "log"
-    "os"
+	"github.com/ftrvxmtrx/tga"
+	"image"
+	"image/color"
+	"log"
+	"os"
 )
 
 func main() {
-    img := image.NewRGBA(image.Rect(0, 0, 10, 10))
+	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
+    RGBAWhite := color.RGBA{0xff, 0xff, 0xff, 0xff}
+    RGBARed   := color.RGBA{0xff, 0x88, 0x88, 0xff}
 
-    file, err := os.Create("simple.tga")
-    if err != nil {
-        log.Fatal("err")
-    }
-    defer file.Close()
+	for y := img.Rect.Min.Y; y < img.Rect.Max.Y; y++ {
+		for x := img.Rect.Min.X; x < img.Rect.Max.X; x++ {
+			img.Set(x, y, RGBAWhite)
+		}
+	}
+    img.Set(30, 40, RGBARed)
 
-    tga.Encode(file, img)
+	file, err := os.Create("simple.tga")
+	if err != nil {
+		log.Fatal("err")
+	}
+	defer file.Close()
 
-    log.Print("Finish!")
+	tga.Encode(file, img)
+
+	log.Print("Finish!")
 }
