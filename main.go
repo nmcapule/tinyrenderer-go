e747f1eef207fc1ee28812c7e63dbe63d0abc2ca
Nathaniel M. Capule <nmcapule@gmail.com>
2016-09-20 22:28:09 +0800

    Add model triangle rendering.

--- main.go
+++ main.go
@@ -96,6 +96,64 @@ func ImageDrawLine(img *image.RGBA, x0, y0, x1, y1 float64, c color.RGBA) {
 	}
 }
 
+func ImageDrawTriangle(img *image.RGBA, p0, p1, p2 Point, c color.RGBA) {
+    // Sort
+    if p0.Y > p1.Y {
+        tmp := p0
+        p0 = p1
+        p1 = tmp
+    }
+    if p0.Y > p2.Y {
+        tmp := p0
+        p0 = p2
+        p2 = tmp
+    }
+    if p1.Y > p2.Y {
+        tmp := p1
+        p1 = p2
+        p2 = tmp
+    }
+
+    // Origin
+    plo := p0
+    pld := p1
+
+    pro := p0
+    prd := p2
+
+    if pld.X > prd.X {
+        tmp := pld
+        pld = prd
+        prd = tmp
+    }
+
+    it := p2.Y - p0.Y
+    for i := 0.; i < it; i++ {
+        y := p0.Y + i
+        if y == pld.Y {
+            plo = pld
+            pld = prd
+        } else if y == prd.Y {
+            pro = prd
+            prd = pld
+        }
+
+        // Get line from x0 to x1
+        x0 := (i - (plo.Y - p0.Y)) * (pld.X - plo.X) / (pld.Y - plo.Y) + plo.X
+        x1 := (i - (pro.Y - p0.Y)) * (prd.X - pro.X) / (prd.Y - pro.Y) + pro.X
+
+        if x0 > x1 {
+            tmp := x0
+            x0 = x1
+            x1 = tmp
+        }
+
+        for x := x0; x < x1; x++ {
+            img.Set(int(x), int(y), c)
+        }
+    }
+}
+
 func ImageRenderWireframeModel(img *image.RGBA, m *Model) {
 	RGBAWhite := color.RGBA{0xff, 0xff, 0xff, 0xff}
     width := img.Rect.Dx()
@@ -118,22 +176,51 @@ func ImageRenderWireframeModel(img *image.RGBA, m *Model) {
     }
 }
 
+func ImageRenderTriangleModel(img *image.RGBA, m *Model) {
+    width := img.Rect.Dx()
+    height := img.Rect.Dy()
+
+	RGBARed := color.RGBA{0xff, 0x88, 0x88, 0xff}
+    for f := range m.Faces {
+        a := []Point{
+            m.Vertices[m.Faces[f].A],
+            m.Vertices[m.Faces[f].B],
+            m.Vertices[m.Faces[f].C],
+        }
+        ImageDrawTriangle(img,
+            Point{
+                (a[0].X)*float64(width/2) + float64(img.Rect.Min.X+width/2),
+                (a[0].Y)*float64(height/2) + float64(img.Rect.Min.Y+height/2),
+                0,
+            }, Point{
+                (a[0].X + 0.01)*float64(width/2) + float64(img.Rect.Min.X+width/2),
+                (a[0].Y)*float64(height/2) + float64(img.Rect.Min.Y+height/2),
+                0,
+            }, Point{
+                (a[0].X + 0.01)*float64(width/2) + float64(img.Rect.Min.X+width/2),
+                (a[0].Y + 0.01)*float64(height/2) + float64(img.Rect.Min.Y+height/2),
+                0,
+            }, RGBARed)
+    }
+}
+
 func main() {
 	img := image.NewRGBA(image.Rect(0, 0, 800, 800))
 	RGBABlack := color.RGBA{0x00, 0x00, 0x00, 0xff}
-	RGBARed := color.RGBA{0xff, 0x88, 0x88, 0xff}
+	// RGBARed := color.RGBA{0xff, 0x88, 0x88, 0xff}
 
 	for y := img.Rect.Min.Y; y < img.Rect.Max.Y; y++ {
 		for x := img.Rect.Min.X; x < img.Rect.Max.X; x++ {
 			img.Set(x, y, RGBABlack)
 		}
 	}
-	img.Set(30, 40, RGBARed)
 
 	model := Model{}
 	model.ReadObj("obj/african_head.obj")
-    ImageRenderWireframeModel(img, &model)
+    // ImageRenderWireframeModel(img, &model)
+    ImageRenderTriangleModel(img, &model)
 	ImageFlipVertical(img)
+    // ImageDrawTriangle(img, Point{100, 180, 0}, Point{400, 150, 0}, Point{40, 400, 0}, RGBARed)
 
 	file, err := os.Create(ImageOutFile)
 	if err != nil {
